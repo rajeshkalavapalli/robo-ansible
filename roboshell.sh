a1132e1878ceb4c8c306d22925a9f367afc92614
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-04-03 16:11:24 +0530

    final change

--- roboshell.sh
+++ roboshell.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-AMI=ami-0f3c7d07486cad139 #this keeps on changing
-SG_ID=sg-0ecc0d38b1974ad3f #replace with your SG ID
+AMI="ami-0f3c7d07486cad139" # this keeps on changing
+SG_ID="sg-0ecc0d38b1974ad3f" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z07107583SIBMEYCNYDA6 # replace your zone ID
+ZONE_ID="Z07107583SIBMEYCNYDA6" # replace your zone ID
 DOMAIN_NAME="bigmatrix.in"
 
 for i in "${INSTANCES[@]}"
@@ -15,26 +15,25 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text --no-cli-pager
+    IP_ADDRESS=$(aws ec2 run-instances --image-id "$AMI" --instance-type "$INSTANCE_TYPE" --security-group-ids "$SG_ID" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text --no-cli-pager)
     echo "$i: $IP_ADDRESS"
 
-    #create R53 record, make sure you delete existing record
+    # Create R53 record, make sure you delete existing record
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "CREATE"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$i'.'$DOMAIN_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP_ADDRESS'"
-            }]
+    --hosted-zone-id "$ZONE_ID" \
+    --change-batch "$(cat <<EOF
+{
+    "Comment": "Creating a record set for cognito endpoint",
+    "Changes": [{
+        "Action": "CREATE",
+        "ResourceRecordSet": {
+            "Name": "$i.$DOMAIN_NAME",
+            "Type": "A",
+            "TTL": 300,
+            "ResourceRecords": [{"Value": "$IP_ADDRESS"}]
         }
-        }]
-    }
-        '
+    }]
+}
+EOF
+)"
 done
