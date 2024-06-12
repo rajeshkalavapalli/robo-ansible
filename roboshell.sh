79e8046d6bdffbfbca27f7aabfcf0ed46418d461
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-06-12 18:25:23 +0530

    sg

--- roboshell.sh
+++ roboshell.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-AMI="ami-031d574cddc5bb371" #this keeps on changing
-SG_ID="sg-0918fbe51ab2db638" #replace with your SG ID
+AMI=ami-031d574cddc5bb371 #this keeps on changing
+SG_ID=sg-0918fbe51ab2db638 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID="Z02400681RG5QICC3BHZN" # replace your zone ID
+ZONE_ID=Z02400681RG5QICC3BHZN # replace your zone ID
 DOMAIN_NAME="bigmatrix.in"
 
 for i in "${INSTANCES[@]}"
@@ -15,7 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-0918fbe51ab2db638 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
     #create R53 record, make sure you delete existing record
