0bcb29b34aff8d9848de6d4ef96a1bf30fdc82c4
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-06-12 16:18:21 +0530

    new

--- roboshell.sh
+++ roboshell.sh
@@ -1,49 +1,29 @@
 #!/bin/bash
 
-# AMI ID, Security Group ID, Zone ID, and Domain Name
-AMI="ami-031d574cddc5bb371" # This should be dynamically updated if it changes often
-SG_ID="sg-0918fbe51ab2db638" # Replace with your Security Group ID
-ZONE_ID="Z02400681RG5QICC3BHZN" # Replace with your Route 53 Zone ID
+AMI="ami-031d574cddc5bb371" # this keeps on changing
+SG_ID="sg-0ecc0d38b1974ad3f" # replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID="Z07107583SIBMEYCNYDA6" # replace your zone ID
 DOMAIN_NAME="bigmatrix.in"
 
-# Array of instance names
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-
-# Loop through each instance name
 for i in "${INSTANCES[@]}"
 do
-    # Determine instance type based on instance name
-    if [ "$i" == "mongodb" ] || [ "$i" == "mysql" ] || [ "$i" == "shipping" ]; then
+    if [ "$i" == "mongodb" ] || [ "$i" == "mysql" ] || [ "$i" == "shipping" ]
+    then
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
     fi
 
-    # Launch the EC2 instance and capture its private IP address
-    IP_ADDRESS=$(aws ec2 run-instances \
-        --image-id "$AMI" \
-        --instance-type "$INSTANCE_TYPE" \
-        --security-group-ids "$SG_ID" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" \
-        --query 'Instances[0].PrivateIpAddress' \
-        --output text \
-        --no-cli-pager)
-    
-    # Check if the IP_ADDRESS variable is empty, which indicates a failure in instance creation
-    if [ -z "$IP_ADDRESS" ]; then
-        echo "Failed to launch instance for $i. Skipping DNS record creation."
-        continue
-    fi
-
-    # Output the instance name and its IP address
+    IP_ADDRESS=$(aws ec2 run-instances --image-id "$AMI" --instance-type "$INSTANCE_TYPE" --security-group-ids "$SG_ID" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text --no-cli-pager)
     echo "$i: $IP_ADDRESS"
 
-    # Create or update Route 53 DNS record
+    # Create R53 record, make sure you delete existing record
     aws route53 change-resource-record-sets \
-        --hosted-zone-id "$ZONE_ID" \
-        --change-batch "$(cat <<EOF
+    --hosted-zone-id "$ZONE_ID" \
+    --change-batch "$(cat <<EOF
 {
-    "Comment": "Creating or updating a record set for $i.$DOMAIN_NAME",
+    "Comment": "Creating a record set for cognito endpoint",
     "Changes": [{
         "Action": "UPSERT",
         "ResourceRecordSet": {
